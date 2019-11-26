3ca505b8bbebea094e3f1b15055ceeaa580ccf5d
Vad1m21 <povar.vadim@gmail.com>
2019-11-26 18:36:04 +0200

    initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("out");
+        System.out.println("Как я провел лето\n -провел лето взглядом\n -никак");
     }
 }
